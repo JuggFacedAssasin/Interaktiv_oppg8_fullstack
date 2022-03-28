4b5ed35a2dec66a0adcb8ea7f589fe4108145faa
JuggFacedAssasin <jorgenboe@hotmail.com>
2022-03-28 10:00:10 +0200

    git uppdate mandag 28 mars

--- react_oppg8/src/components/Actor.js
+++ react_oppg8/src/components/Actor.js
@@ -13,7 +13,13 @@ const Actor = ({ dataA }) => {
       <img src={actorBioArray.portrait.asset.url} alt="missing portrait"></img>
       <h2>{actorBioArray.fullname}</h2>
       <p><strong>Professsion: </strong>{actorBioArray._type}</p>
-      <p><strong>Apeared in: </strong>{actorBioArray.movierole}, {actorBioArray.movierole2}, {actorBioArray.movierole3}</p>
+        <p>
+          <h3><strong>Apeared in: </strong></h3>
+          <p className="borderNone">{actorBioArray.movierole}</p>
+          <p className="borderNone">{actorBioArray.movierole2}</p>
+          <p className="borderNone">{actorBioArray.movierole3}</p>
+        </p>
+      
       <p><strong>Mini bio: </strong><br></br>{actorBioArray.bio}</p>
       <button><Link to="/Actors">Back</Link></button>
     </article>
--- react_oppg8/src/components/Movie.js
+++ react_oppg8/src/components/Movie.js
@@ -13,7 +13,12 @@ const Movie = ({ title, actorm, dataM }) => {
       <img src={movieSiteArray.poster.asset.url} alt="Movie poster missing"></img>
       <h2>{ movieSiteArray.title }</h2>
       <p><strong>Released in </strong>{ movieSiteArray.year }</p>
-      <p><strong>Actors apearing in the movie: </strong> { movieSiteArray.actor } - {movieSiteArray.actor2 }</p>
+        <p>
+          <h3><strong>Actors apearing in the movie: </strong></h3>
+          <p className="borderNone">{ movieSiteArray.actor }</p>
+          <p className="borderNone">{movieSiteArray.actor2 }</p>
+        </p>
+
       <p><strong>Moviedescription: </strong><br></br>{movieSiteArray.description}</p>
       <button><Link to="/Movies">Back</Link></button>
     </article>
